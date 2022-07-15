6e4cceb1b825f5f74d200537f1ed925a5f1a423b
thals1214 <thaxls@ewhain.net>
2022-07-15 20:00:14 +0900

    url-minor fix

--- cari/urls.py
+++ cari/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import ResultDetail, UserInfo
 
 urlpatterns = [
-    path('cari/image', UserInfo.as_view()),
-    path('cari/result', ResultDetail.as_view())
+    path('cari/image', UserInfo.as_view(), name='cari_image'),
+    path('cari/result', ResultDetail.as_view(), name='cari_result')
 ]
