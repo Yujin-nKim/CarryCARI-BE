3a55ce2b9b4f7dc56fe21d69b0864eba251f93ce
thals1214 <thaxls@ewhain.net>
2022-07-15 22:56:23 +0900

    email - adjusted urls

--- cari/urls.py
+++ cari/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from .views import ResultDetail, UserInfo
+from .views import ResultDetail, UserInfo, SendEmail
 from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
     path('cari/image', UserInfo.as_view(), name='cari_image'),
-    path('cari/result', ResultDetail.as_view(), name='cari_result')
+    path('cari/result', ResultDetail.as_view(), name='cari_result'),
+    path('cari/email', SendEmail.as_view(), name='cari_email')
 ]
 
 # image conversion
