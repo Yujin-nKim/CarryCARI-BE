69557c2202765ac3f4f2c6d7a5ff7f0eda8418c5
Yujin_nKim <yujin.kimd@gmail.com>
2022-07-18 16:06:24 +0900

    fix: fix error

--- cari/models.py
+++ cari/models.py
@@ -20,7 +20,7 @@ class User(models.Model):
 # 결과물 이미지 저장
 class Result(models.Model):
     result_id = models.AutoField(primary_key=True)
-    result_img = models.ImageField(blank=True, upload_to='_media/result_images/')
+    result_img = models.ImageField(blank=True, upload_to='result_images/')
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
     result_emotion = models.IntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
